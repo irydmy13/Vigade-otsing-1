253804b5a2b234104af677c43ca696bfb055c65c
irydmy13 <kotiukir@gmail.com>
2024-12-24 19:54:52 +0200

    Praktiline töö: "Matemaatika"

--- Praktiline töö.  Matemaatika.py
+++ Praktiline töö.  Matemaatika.py
@@ -1,14 +1,10 @@
-
-
-#ЗАДАНИЕ В ПРОЦЕССЕ РАБОТЫ
 import random
 
 print("Добро пожаловать в проверку знаний по математике!")
-print("Выберите уровень сложности: 1(лёгкий), 2(средний), 3(сложный)")
 
 while True:
     try:
-        level = int(input("Введите уровень сложности 1, 2 или 3:"))
+        level = int(input("Введите уровень сложности 1, 2 или 3: "))
         if level in [1, 2, 3]:
             break
         else:
@@ -18,7 +14,7 @@ while True:
 
 while True:
     try:
-        num_ques = int(input("Сколько примеров вы хотите решить?"))
+        num_ques = int(input("Сколько примеров вы хотите решить? "))
         if num_ques > 0:
             break
         else:
@@ -26,86 +22,66 @@ while True:
     except ValueError:
         print("Введите целое число.")
 
+correct_count = 0
+incorrect_count = 0 #Инициализация счетчиков
+
+for _ in range(num_ques):
+    if level == 1:
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
         operations = ['+', '-']
     elif level == 2:
-        num1 = random.randint(1, 50)    
+        num1 = random.randint(1, 50)
         num2 = random.randint(1, 50)
         operations = ['+', '-', '*']
-    else:  # level 3
+    else:
         num1 = random.randint(1, 100)
         num2 = random.randint(1, 100)
         operations = ['+', '-', '*', '/']
 
-    operation = random.choice(operations)
+    operation = random.choice(operations) #Выбор случайной операции
 
     if operation == '/':
-        num1 = num1 * num2 #Избегаем деления на 0 и делаем целочисленные примеры
+        num1 = num1 * num2
         question = f"{num1} / {num2}"
         correct_answer = num1 // num2
-    else:
-        question = f"{num1} {operation} {num2}"
-        correct_answer = eval(question)
-
-    return question, correct_answer
-
-def main():
-    print("Добро пожаловать в тест по математике!")
-    print("Выберите уровень сложности: 1 (легкий), 2 (средний), 3 (сложный)")
-
-    while True:
-        try:
-            level = int(input("Введите уровень сложности (1, 2 или 3): "))
-            if level in [1, 2, 3]:
-                break
-            else:
-                print("Пожалуйста, выберите 1, 2 или 3.")
-        except ValueError:
-            print("Введите число 1, 2 или 3.")
+    elif operation == '+':
+        question = f"{num1} + {num2}"
+        correct_answer = num1 + num2
+    elif operation == '-':
+        question = f"{num1} - {num2}"
+        correct_answer = num1 - num2
+    elif operation == '*':
+        question = f"{num1} * {num2}"
+        correct_answer = num1 * num2
 
     while True:
         try:
-            num_questions = int(input("Сколько примеров вы хотите решить? "))
-            if num_questions > 0:
-                break
+            user_answer = int(input(f"Решите: {question} = "))
+            if user_answer == correct_answer:
+                print("Правильно!")
+                correct_count += 1  #Увеличиваем счетчик правильных ответов
             else:
-                print("Введите положительное число.")
+                print(f"Неправильно. Правильный ответ: {correct_answer}.")
+                incorrect_count += 1  #Увеличиваем счетчик неправильных ответов
+            break
         except ValueError:
-            print("Введите целое число.")
+            print("Введите числовой ответ.")
 
-    score = 0
+print("\nРезультаты тестирования:")
+print(f"Правильных ответов: {correct_count}")
+print(f"Неправильных ответов: {incorrect_count}")
+print(f"Ваш результат: {correct_count}/{num_ques}")
 
-    for i in range(num_questions):
-        question, correct_answer = generate_question(level)
-        print(f"Пример {i + 1}: {question} = ?")
+score_percentage = (correct_count / num_ques) * 100 #Подсчет процента правильных ответов
+if score_percentage < 60:
+    grade = 2
+elif 60 <= score_percentage < 75:
+    grade = 3
+elif 75 <= score_percentage < 90:
+    grade = 4
+else:
+    grade = 5
 
-        while True:
-            try:
-                user_answer = int(input("Ваш ответ: "))
-                break
-            except ValueError:
-                print("Введите число.")
-
-        if user_answer == correct_answer:
-            print("Правильно!")
-            score += 1
-        else:
-            print(f"Неправильно. Правильный ответ: {correct_answer}")
-
-    percentage = (score / num_questions) * 100
-    print(f"Вы решили {score} из {num_questions} примеров правильно ({percentage:.2f}%).")
-
-    if percentage < 60:
-        grade = 2
-    elif 60 <= percentage < 75:
-        grade = 3
-    elif 75 <= percentage < 90:
-        grade = 4
-    else:
-        grade = 5
-
-    print(f"Ваша оценка: Hinne {grade}")
-
-if __name__ == "__main__":
-    main()
+print(f"Процент правильных ответов: {score_percentage:.2f}%")
+print(f"Ваша оценка: {grade}")
