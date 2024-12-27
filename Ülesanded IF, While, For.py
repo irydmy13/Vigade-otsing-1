e2902566f127cfbbada78d567ecdd7ea0a30b9d9
irydmy13 <kotiukir@gmail.com>
2024-12-27 16:46:53 +0200

    ules 3

--- Ülesanded IF, While, For.py
+++ Ülesanded IF, While, For.py
@@ -2,16 +2,29 @@
 # N = int(input("Введите количество чисел для ввода: "))
 
 # if N > 0:
-#     M = max(int(input(f"Введите число {i + 1}: ")) for i in range(N)) # M-максимальное
+#     M = max(int(input(f"Введите число {i + 1}: ")) for i in range(N)) #M-максимальное
 #     print(f"Максимальное число: {M}")
 # else:
 #     print("Количество чисел должно быть больше 0.")
 
-#ules 2
-while True:
-    A = int(input("Введите целое число: "))
-    if A == 13:
-        print(77)
-    else:
-        print(A)
+# #ules 2
+# while True:
+#     A = int(input("Введите целое число: "))
+#     if A == 13:
+#         print(77)
+#     else:
+#         print(A)
+
+# #ules 3
+# start = 10 #км
+# days = 7
+
+# summ = 0
+# current_distance = start
+
+# for _ in range(days):
+#     summ += current_distance
+#     current_distance += current_distance * (10 / 100)
+
+# print(f"Суммарный путь за {days} дней: {summ:.2f} км")
 
