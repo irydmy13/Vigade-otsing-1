4ba5087264cfc2eab5b1d365dea85eca534983f5
irydmy13 <kotiukir@gmail.com>
2025-01-22 22:43:18 +0200

    ulesanne3

--- Tund7(Def).py
+++ Tund7(Def).py
@@ -1,7 +1,25 @@
 from Tund7Modul import *
-a=int(input("Sisesta arv1: "))
-b=int(input("Sisesta arv2: "))
-c=input("Sisesta arv3: ")
+# a=int(input("Sisesta arv1: "))
+# b=int(input("Sisesta arv2: "))
+# c=input("Sisesta arv3: ")
 
-vastus=summa3(a,b,int(c))
-print(vastus)
+# vastus=summa3(a,b,int(c))
+# print(vastus)
+
+# #ulesanne1
+# a=float(input("Sisesta arv1: "))
+# b=float(input("Sisesta arv2: "))
+# t=str(input("Sisesta atitmeetiline tehing: "))
+# vastus = arithmetic(a, b, c)
+# print(vastus)
+
+# #ulesanne2
+# aasta=int(input("Sisestage aasta, et kontrollida, kas see on liigaasta: "))
+#v=is_year_leap()
+# print(v)
+
+# # #ulesanne3
+# a = float(input("Sisesta a = "))
+# print(square_text(a:float))
+
+
--- Tund7Modul.py
+++ Tund7Modul.py
@@ -1,14 +1,87 @@
-from asyncio import AbstractEventLoop
+from random import *
+# from asyncio import AbstractEventLoop
 
 
-def summa3(arv1:int,arv2:int,arv3:int)->int:
-    """Tagastab kolme taisarvu summa
+# def summa3(arv1:int,arv2:int,arv3:int)->int:
+#     """Tagastab kolme taisarvu summa
 
-    :param int arv1: Esimene number
-    :param int arv2: Teine number
-    :param int arv3: Kolmas number
-    :rtype: int
+#     :param int arv1: Esimene number
+#     :param int arv2: Teine number
+#     :param int arv3: Kolmas number
+#     :rtype: int
 
-    """
-    summa=arv1+arv2+arv3
-    return summa
+#     """
+#     summa=arv1+arv2+arv3
+#     return summa
+
+# #ulesanne1
+# def arithmetic(a:float, b:float, t:str)->any:
+#     """Lihtne kalkulaator.
+#     + - liitmine
+#     - - lahutamine
+#     * - korrutamine
+#     / - jagamine
+#     :param float a: arv1
+#     :param float b: arv2
+#     :param str t: atitmeetiline tehing
+#     :rtype: var Maaramata tuup(float or str)
+#     """
+#     if t in ["+","-","*","/"]:
+#         if b==0 and t=="/":
+#             vastus="DIV/0"
+#         else:
+#             vastus=eval(str(a)+t+str(b))
+#     else:
+#         vastus="Tundmatu tehe"
+
+#     return vastus
+
+# #ulesanne2
+# def is_year_leap(aasta:int)->bool:
+
+#     """Liigaasta leidmine
+#     Tagastab True, kui liigaasts ja False kui on tavaline aasta.
+#     :param int asta: aasta number
+#     :rtype: bool tagastab loogilises formaadis tulemus
+#     """
+
+#     if aasta%4==0:
+#         v=True
+#     else:
+#         v=False
+#     return v
+
+# #ulesanne3
+# def square_text(a:float)->str:
+#     """Ruut
+
+#     :Kirjutage funktsiooni ruut, mis votab 1 argumendi - ruudu 
+#     :kulje ja tagastab 3 vaartust: ruudu umbermoot, 
+#     :ruudu pindala ja ruudu diagonaal.
+    
+#     """
+#     S=a**2
+#     P=4*a
+#     d=(2)**(1/2)*a
+#     return (f"S: {S}, P: {P}, d: {round(d,2)}")
+
+# #ulesanne4
+# def season(param:int)->str:
+#     """
+#     """
+#     if 1<=param<=12:
+#         if param in [1,2,12]:
+#             v="talv"
+#         elif param>2 and param<6:
+#             v="kevad"
+#         elif 6<=param<=8:
+#             v="suvi"
+#         else:
+#             v="sugis"
+#     else:
+#         v="!!!!"
+#     return v
+
+#ulesanne5
+
+
