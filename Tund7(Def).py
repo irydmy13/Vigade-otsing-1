e6e2deaf640b9531b63fd304c50f01f14bb54576
irydmy13 <kotiukir@gmail.com>
2025-01-22 14:27:26 +0200

    TundDef

---
+++ Tund7(Def).py
@@ -0,0 +1,7 @@
+from Tund7Modul import *
+a=int(input("Sisesta arv1: "))
+b=int(input("Sisesta arv2: "))
+c=input("Sisesta arv3: ")
+
+vastus=summa3(a,b,int(c))
+print(vastus)
---
+++ Tund7Modul.py
@@ -0,0 +1,14 @@
+from asyncio import AbstractEventLoop
+
+
+def summa3(arv1:int,arv2:int,arv3:int)->int:
+    """Tagastab kolme taisarvu summa
+
+    :param int arv1: Esimene number
+    :param int arv2: Teine number
+    :param int arv3: Kolmas number
+    :rtype: int
+
+    """
+    summa=arv1+arv2+arv3
+    return summa
