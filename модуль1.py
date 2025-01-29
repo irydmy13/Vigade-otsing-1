af3d5132a0503a4eb21e5503d4a595280ac882e9
irydmy13 <kotiukir@gmail.com>
2025-01-29 14:41:43 +0200

    IT organisatsiooni juhtimine ja taristu

---
+++ модуль1.py
@@ -0,0 +1,39 @@
+openOrganization = "Open Organization:\nPlussid:\n  1) Flexible\n  2) Innovations\n  3) Communication with world"
+
+closeOrganization = "\nClose Organization:\nPlusses:\n  1) Stability\n  2) More commerce\n  3) Better security"
+
+print(openOrganization, "\n", closeOrganization)
+print("_" * 15, "\n")
+
+comercyOrganization = "Commercial Organizations:\nExamples: Sony, Microsoft, Amazon.\nPlusses:\n  - On võimalus saada kasu.\n  - Konkurents.\n  - Turg."
+
+NoComercialOrganization = "Non-Commercial Organizations:\nExamples: Punane Rist, Greenpeace, Amnesty International."
+
+GovermentOrganization = "Government Organizations:\nExamples: Jails, Omavalitsused.\nRahastamine:\n  - Maksud\n  - Riigieelarve"
+
+educationalOrganization = "Educational Organizations:\nExamples: Koolid, Ülikoolid, Tööstushariduskeskused.\nRahastamine:\n  - Riigieelarve"
+
+startup = "Startups:\nExamples: Bolt, Wise, Pipedrive.\nRahastamine:\n  - Investeerimine"
+
+internationalOrganization = "International Organizations:\nExamples: NATO, OON.\nRahastamine:\n  - Kõik liikmed maksavad määratud tasu."
+
+mediaOrganization = "Media Organizations:\nExamples: ERR, Reporter, Postimees, Delfi.\nRahastamine:\n  - Reklaam"
+
+ReligionOrganization = "Religious Organizations:\nExamples: Katoliku Kirik, Õigeusu Kirik.\nRahastamine:\n  - Riigieelarve\n  - Toetused"
+
+associations = "Associations:\nExamples: Erasmus+.\nRahastamine:\n  - Riigieelarve\n  - Investeerimine"
+
+fonds = "Funds:\nExamples: Bill ja Melinda Gatesi Saastud.\nRahastamine:\n  - Toetused\n  - Investeerimine"
+
+print(comercyOrganization, "\n")
+print(NoComercialOrganization, "\n")
+print(GovermentOrganization, "\n")
+print(educationalOrganization, "\n")
+print(startup, "\n")
+print(internationalOrganization, "\n")
+print(mediaOrganization, "\n")
+print(ReligionOrganization, "\n")
+print(associations, "\n")
+print(fonds)
+
+print("-" * 15)
