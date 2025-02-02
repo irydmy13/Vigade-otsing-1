6dcedc720149d03809f21a7e721dd438229bacb6
irydmy13 <kotiukir@gmail.com>
2025-02-02 23:39:28 +0200

    create pswd

---
+++ Registreerimine ja autoriseerimine.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+users = [] #список пользователей
+passwords = [] #список паролей
+
+def create_pswd():
+    str0 = ".,:;!_*-+()/#¤%&"
+    str1 = '0123456789'
+    str2 = 'qwertyuiopasdfghjklzxcvbnm'
+    str3 = str2.upper()
+    str4 = str0 + str1 + str2 + str3
+    ls = list(str4)
+    random.shuffle(ls)
